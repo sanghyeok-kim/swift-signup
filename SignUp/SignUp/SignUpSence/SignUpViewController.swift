aecb42b34b78b35c2c5ba5108b4abab30852d178
P_SeoB <akdlsjtm21@naver.com>
2022-03-28 17:37:07 +0900

    Feat:Add all InputViewComponent

--- SignUp/SignUp/SignUpSence/SignUpViewController.swift
+++ SignUp/SignUp/SignUpSence/SignUpViewController.swift
@@ -31,15 +31,15 @@ final class SignUpViewController: UIViewController {
     private func configureStackView() {
         let inputViewComponents = configureInputViewComponents()
         
-        stackView.alignment = .fill
-        stackView.distribution = .equalSpacing
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
         
         inputViewComponents.forEach{ inputViewable in
             guard let view = inputViewable as? SignUpInputViewComponent else { return }
             stackView.addArrangedSubview(view)
         }
-        self.view.addSubview(stackView)
         
+        self.view.addSubview(stackView)
         
         stackView.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor,constant: 32).isActive = true
         stackView.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor,constant: -32).isActive = true
@@ -48,14 +48,15 @@ final class SignUpViewController: UIViewController {
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
-
-        
     }
     
     private func configureInputViewComponents() -> [SignUpInputViewable]{
-        let IDComponent = SignUpInputViewFactory.makeSignUpViewComponent(labelText: "아이디", placeHolder: "영문 대/소문자, 숫자, 특수기호(!@#$%) 8~16자")
+        let IDComponent = SignUpInputViewFactory.makeSignUpViewComponent(labelText: "아이디", placeHolder: "영문 대/소문자, 숫자, 특수기호(_,-) 5~20자")
+        let passwordComponet = SignUpInputViewFactory.makeSignUpViewComponent(labelText: "비밀번호", placeHolder: "영문 대/소문자, 숫자, 특수문자(!@#$% 8~16자")
+        let passwordRecheckComponent = SignUpInputViewFactory.makeSignUpViewComponent(labelText: "비밀번호 재확인", placeHolder: "")
+        let nameComponent = SignUpInputViewFactory.makeSignUpViewComponent(labelText: "이름", placeHolder: "")
         
-        return [IDComponent]
+        return [IDComponent,passwordComponet,passwordRecheckComponent,nameComponent]
     }
     
 }
