318ee793fe6fe8ef71e578d59c788e8df9f52576
sanghee <anshee97@naver.com>
2022-03-28 10:49:55 +0900

    Feat: Add Project

---
+++ SignUp/SignUp/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  SignUp
+//
+//  Created by 안상희 on 2022/03/28.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ SignUp/SignUp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SignUp
+//
+//  Created by 안상희 on 2022/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
