d5b49f33c2caf6c1e4cc2ca1e770466355eb26dc
P_SeoB <akdlsjtm21@naver.com>
2022-03-28 14:32:42 +0900

    Feat:Add ID input view

--- SignUp/SignUp/SignUpSence/SignUpViewComponent.swift
+++ SignUp/SignUp/SignUpSence/SignUpViewComponent.swift
@@ -9,7 +9,47 @@ import UIKit
 
 final class SignUpViewComponent:UIView {
     
+    private lazy var label = UILabel()
+    private lazy var textField:UITextField = {
+        let textField = UITextField()
+        textField.layer.cornerRadius = 6.0
+        textField.layer.borderWidth = 1.0
+        return textField
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUp()
+    }
+    
+    
+    private func setUp() {
+        [label,textField].forEach{ self.addSubview($0) }
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        
+        textField.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        textField.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        textField.topAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
+        
+    }
+    
+    func labelText(text:String) {
+        self.label.text = text
+    }
+    
+    func placeholder(text:String) {
+        self.textField.placeholder = text
+    }
     
 }
 
--- SignUp/SignUp/SignUpSence/SignUpViewController.swift
+++ SignUp/SignUp/SignUpSence/SignUpViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 final class SignUpViewController: UIViewController {
     
+    
+    let IDComponent = SignUpViewComponent(frame: .zero)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSignUpView()
+        
     }
     
     private func configureSignUpView() {
         setTitle()
+        configureViewComponents()
     }
     
     private func setTitle() {
@@ -25,6 +31,12 @@ final class SignUpViewController: UIViewController {
         self.navigationItem.titleView = label
     }
     
+    private func configureViewComponents() {
+        IDComponent.labelText(text: "아이디")
+        IDComponent.placeholder(text: "영문 대/소문자, 숫자, 특수기호(!@#$%) 8~16자")
+        
+        [IDComponent].forEach{ self.view.addSubview($0) }
+    }
     
 }
 
