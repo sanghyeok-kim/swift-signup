03cbcfb42c4bc35620b7ddc380be7210ad6f7097
sanghee <anshee97@naver.com>
2022-03-29 10:54:16 +0900

    Feat: Add AgreeViewController and connected with Storyboard Remove: Remove ViewController

---
+++ SignUp/SignUp/Controller/AgreeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AgreeViewController.swift
+//  SignUp
+//
+//  Created by 안상희 on 2022/03/29.
+//
+
+import UIKit
+
+class AgreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
