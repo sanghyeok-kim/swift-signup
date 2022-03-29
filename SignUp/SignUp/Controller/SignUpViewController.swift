2bdd6e987c2b2cf21a4338ea0ba7d28f4f24586c
sanghee <anshee97@naver.com>
2022-03-29 15:46:44 +0900

    Design: Edit Storyboard Add: Connect Outlet

--- SignUp/SignUp/Controller/SignUpViewController.swift
+++ SignUp/SignUp/Controller/SignUpViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordCheckTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
