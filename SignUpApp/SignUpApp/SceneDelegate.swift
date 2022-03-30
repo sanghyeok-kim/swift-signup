23827695cb28ad401a1e17210031d355a4a68e4e
sanghyeok-kim <slsnsi1212@gmail.com>
2022-03-30 14:28:12 +0900

    [Add] 프로젝트 생성 및 초기 설정

---
+++ SignUpApp/SignUpApp/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  SignUpApp
+//
+//  Created by 김상혁 on 2022/03/30.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
